4a2948aff54aab5005586fe8bd8029455f123592
Panikiran <pvpkiran@gmail.com>
2019-06-12 11:32:00 +0200

    Changing welcome message

--- src/main/java/in/phani/springboot/dockerjib/controller/HelloController.java
+++ src/main/java/in/phani/springboot/dockerjib/controller/HelloController.java
@@ -3,11 +3,19 @@ package in.phani.springboot.dockerjib.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello";
+        try {
+            return "Hello Muddu Gundu. You are being served from "+ InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return "Hello Muddu Gundu";
     }
 }
